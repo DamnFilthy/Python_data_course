47c621410bfa98c8408fdc470b72804adaaf959a
DamnFilthy <aleksandr-shiryaev-1993@mail.ru>
2020-09-28 11:26:35 +0300

    finish first task

--- py_data_homework/py_da_hw_3/files/json_learning.py
+++ py_data_homework/py_da_hw_3/files/json_learning.py
@@ -1,14 +1,16 @@
 import json
 
 purchases = {}
-i = 0
-f = open('purchase_log.txt', 'r', encoding = 'UTF-8')
+f = open(r'C:\Users\User\Desktop\purchase_log.txt', 'r', encoding = 'UTF-8')
 d = open('results.txt', 'w')
+next(f)
 for element, line in enumerate(f):
     line = line.strip()
     dict_ = json.loads(line)
-    purchases = {dict_['user_id'] , dict_['category'] }
-    d.write(str(purchases))
-    d.write('\n')
+    purchases.update({dict_['user_id'] : dict_['category'] })
 
+d.write(str(purchases))
+d.write('\n')
 d.close()
+
+print(purchases)
